a46eba5370f3878b3c0edac406988a9ec673ed02
wheleph <wheleph@gmail.com>
2020-07-21 19:32:13 +0300

    Bump version

--- nttt/__init__.py
+++ nttt/__init__.py
@@ -3,11 +3,11 @@ from .tidyup import tidyup_translations
 def main():
     from argparse import ArgumentParser
 
-    parser = ArgumentParser(description="Nina's Translation Tidyup Tool v 0.1.0")
+    parser = ArgumentParser(description="Nina's Translation Tidyup Tool v 0.1.1-SNAPSHOT")
     parser.add_argument("-i", "--input", help="The input directory which contains the content to tidy up, defaults to the current directory.")
     parser.add_argument("-o", "--output", help="The output directory where the upgraded content should be written, defaults to the same as directory.")
     args = parser.parse_args()
-    
+
     if args.input:
         input_folder = args.input
     else:
