d94c09a7bafe1b0f7aa38569270d3d3393b16aaf
wheleph <wheleph@gmail.com>
2020-07-28 08:50:02 +0300

    Improve file operations in tests

--- unit_test/test_eol.py
+++ unit_test/test_eol.py
@@ -25,14 +25,13 @@ class TestEol(unittest.TestCase):
     #                       "  - title: Inleiding\n")
 
     def helper_test_fix_metadata(self, input_file_content, expected_output_file_content):
-        with tempfile.NamedTemporaryFile() as temp_src, \
-                tempfile.NamedTemporaryFile() as temp_dest:
+        with tempfile.NamedTemporaryFile(mode="wb") as temp_src, \
+                tempfile.NamedTemporaryFile(mode="rb") as temp_dest:
             temp_src.write(input_file_content.encode('utf-8'))
             temp_src.flush()
 
             nttt.tidyup.fix_meta(temp_src.name, temp_dest.name)
 
-            temp_dest.seek(0)
             result = temp_dest.read().decode('utf-8')
             self.assertEqual(result, expected_output_file_content)
 
