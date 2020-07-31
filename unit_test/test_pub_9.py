fff754dec812fda704bb23243c52dfd92f72694c
Manos Zeakis <ezeakis@gmail.com>
2020-07-31 10:43:36 +0300

    Create test_pub_9.py

---
+++ unit_test/test_pub_9.py
@@ -0,0 +1,17 @@
+import unittest
+import nttt
+
+
+class TestPub9(unittest.TestCase):
+    def test_pub_9(self):
+    
+        c_initial = 'asd _ fgh _ asd ` ghj ` asd ** hjk ** asd * uio * asd'
+        c_target = 'asd _fgh_ asd `ghj` asd **hjk** asd *uio* asd'
+        
+        self.assertEqual(nttt.trim_pub_9(c_initial), c_target)
+    
+
+
+if __name__ == '__main__':
+    unittest.main()
+    print("hi")
