ff3b8f6a87d1e0b50d5c08caba8fb21a7655bdeb
ezeakis <ezeakis@gmail.com>
2020-09-14 06:30:25 +0300

    Update cleanup_markdown.py

--- nttt/cleanup_markdown.py
+++ nttt/cleanup_markdown.py
@@ -1,8 +1,19 @@
 import re
+import logging
+logging.basicConfig(filename='example.log',level=logging.INFO)
 
 
 def trim_md_tags(file_content):
-    return re.sub(r'(?P<tag>`|_{1,3}|\*{1,3})(?P<content>.+?)(?P=tag)', replacement_builder, file_content)
+    myregex = r'(?P<tag>`|_{1,3}|\*{1,3})(?P<content>.+?)(?P=tag)'
+    result = re.sub(myregex, replacement_builder, file_content)
+    delta = re.findall(myregex, file_content)
+    delta2 = ['{0}{1}{0}'.format(i[0], i[1]) for i in delta]
+    delta3 = [re.sub(myregex, replacement_builder, i) for i in delta2]
+    for i, j in zip(delta2, delta3):
+        logging.info('New substitution')
+        logging.info(i)
+        logging.info(j)
+    return result
 
 
 def replacement_builder(matchobj):
