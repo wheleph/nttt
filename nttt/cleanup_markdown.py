64eb671e035a9f8c6ed87aa2549bfab8f26c4ebd
ezeakis <ezeakis@gmail.com>
2020-09-17 06:45:24 +0300

    Update cleanup_markdown.py

--- nttt/cleanup_markdown.py
+++ nttt/cleanup_markdown.py
@@ -1,22 +1,18 @@
 import re
-import logging
-logging.basicConfig(filename='example.log',level=logging.INFO)
-
+filename = 'example.log'
 
 def trim_md_tags(file_content):
-    myregex = r'(?P<tag>`|_{1,3}|\*{1,3})(?P<content>.+?)(?P=tag)'
-    result = re.sub(myregex, replacement_builder, file_content)
-    delta = re.findall(myregex, file_content)
-    delta2 = ['{0}{1}{0}'.format(i[0], i[1]) for i in delta]
-    delta3 = [re.sub(myregex, replacement_builder, i) for i in delta2]
-    for i, j in zip(delta2, delta3):
-        logging.info('New substitution')
-        logging.info(i)
-        logging.info(j)
-    return result
+    return re.sub(r'(?P<tag>`|_{1,3}|\*{1,3})(?P<content>.+?)(?P=tag)', replacement_builder, file_content)
 
 
 def replacement_builder(matchobj):
     tag_name = matchobj.group("tag")
     stripped_content = matchobj.group("content").strip()
+    if matchobj.group("content") != stripped_content:
+        with open(filename, "a") as myfile:
+            myfile.write("Tag: {}\n".format(tag_name))
+            myfile.write("{}\n".format(matchobj.string))
+            myfile.write("{}{}{}\n".format(tag_name, matchobj.group("content"), tag_name))
+            myfile.write("{}{}{}\n".format(tag_name, stripped_content, tag_name))
+            myfile.write("\n")
     return "{}{}{}".format(tag_name, stripped_content, tag_name)
