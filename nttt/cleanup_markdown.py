0f39d374b463c5b044dc9a605fb765e0646e6c0d
robertjank <robert-jan.kempenaar@live.com>
2020-09-18 15:40:16 +0200

    Added UTF-8 encoding and simplified logging output.

--- nttt/cleanup_markdown.py
+++ nttt/cleanup_markdown.py
@@ -9,10 +9,8 @@ def replacement_builder(matchobj):
     tag_name = matchobj.group("tag")
     stripped_content = matchobj.group("content").strip()
     if matchobj.group("content") != stripped_content:
-        with open(filename, "a") as myfile:
-            myfile.write("Tag: {}\n".format(tag_name))
-            myfile.write("{}\n".format(matchobj.string))
-            myfile.write("{}{}{}\n".format(tag_name, matchobj.group("content"), tag_name))
-            myfile.write("{}{}{}\n".format(tag_name, stripped_content, tag_name))
-            myfile.write("\n")
+        with open(filename, "a", encoding='utf-8') as myfile:
+            myfile.write("Tag name: {}\n".format(tag_name))
+            myfile.write("Replaced: {}{}{}\n".format(tag_name, matchobj.group("content"), tag_name))
+            myfile.write("    with: {}{}{}\n".format(tag_name, stripped_content, tag_name))
     return "{}{}{}".format(tag_name, stripped_content, tag_name)
