410ccac7d6b549c2c5625889fb1fcc3f44c4dd3a
wheleph <wheleph@gmail.com>
2020-09-23 19:36:22 +0300

    Remove redundant newlines in logging output

--- nttt/nttt_logging.py
+++ nttt/nttt_logging.py
@@ -1,10 +1,10 @@
 def display_md(tag_name, text_before, text_after):
     if text_before != text_after:
-        print("Replaced: {}{}{}\n".format(tag_name, text_before, tag_name))
-        print("    with: {}{}{}\n".format(tag_name, text_after, tag_name))
+        print("Replaced: {}{}{}".format(tag_name, text_before, tag_name))
+        print("    with: {}{}{}".format(tag_name, text_after, tag_name))
+
 
 def display_tags(tag_name, text_before, text_after):
     if text_before != text_after:
-        print("Replaced: <{}>{}</{}>\n".format(tag_name, text_before, tag_name))
-        print("    with: <{}>{}</{}>\n".format(tag_name, text_after, tag_name))
-    
+        print("Replaced: <{}>{}</{}>".format(tag_name, text_before, tag_name))
+        print("    with: <{}>{}</{}>".format(tag_name, text_after, tag_name))
