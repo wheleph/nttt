8c5e3d0196f75ff818ce5906693e84b89ea4006c
wheleph <wheleph@gmail.com>
2020-09-26 21:44:11 +0300

    Keep text inside backquotes intact

--- nttt/cleanup_markdown.py
+++ nttt/cleanup_markdown.py
@@ -2,8 +2,8 @@ import re
 from .nttt_logging import display_md
 
 
-def trim_md_tags(file_content, logging):
-    return re.sub(r'(?P<tag>`|_{1,3}|\*{1,3})(?P<content>.+?)(?P=tag)', replacement_builder(logging), file_content)
+def trim_md_tags(md_file_content, logging):
+    return re.sub(r'(?P<tag>`|_{1,3}|\*{1,3})(?P<content>.+?)(?P=tag)', replacement_builder(logging), md_file_content)
 
 
 def replacement_builder(logging):
--- nttt/tag_trimming.py
+++ nttt/tag_trimming.py
@@ -2,8 +2,23 @@ import re
 from .nttt_logging import display_tags
 
 
-def trim_tags(file_content, logging):
-    return re.sub(r'<(?P<tag>[\w\d]+?)>(?P<content>.+?)</(?P=tag)>', replacement_builder(logging), file_content)
+def trim_html_tags(md_file_content, logging):
+    # The idea of this loop is to detect text that goes inside backquotes (`) and keep that text unchanged.
+    # This is because in Markdown files backquotes enclose code snippets that usually should be not be changed.
+    # We achieve this by splitting the content by backquotes and applying html trimming to every other part.
+    parts = md_file_content.split("`")
+    trimmed_parts = []
+    for i in range(len(parts)):
+        if (i % 2) == 0:
+            trimmed_parts.append(__trim_html_tags(parts[i], logging))
+        else:
+            trimmed_parts.append(parts[i])
+
+    return '`'.join(trimmed_parts)
+
+
+def __trim_html_tags(s, logging):
+    return re.sub(r'<(?P<tag>[\w\d]+?)>(?P<content>.+?)</(?P=tag)>', replacement_builder(logging), s)
 
 
 def replacement_builder(logging):
